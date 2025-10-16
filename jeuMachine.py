ac775d329f3bb1e38618d7cc9bc95f6b6b14ff4d
Xanooo <tamineha@yahoo.fr>
2025-10-16 09:40:10 +0200

    feat: Jeu de Nim fonctionnel pour un humain.

--- jeuMachine.py
+++ jeuMachine.py
@@ -1,41 +1,50 @@
-def update(nb_allumettes):
+def update(nb_matches):
     board=[]
-    for _ in range(nb_allumettes):
+    for _ in range(nb_matches):
         board.append("/")
-        board.append(" ")
     return board
 
 
-def jeu():
-
-    jouer = True
-    nombre_allumettes=8
-    board = [] 
-    gobelets = [] 
-    print("Qui va jouer en premier ? (1 pour l'ordinateur, 2 pour vous)")
-    choix = input()
-    if choix == "1":
-        player = "MACHINE"
-    else:
-        player = "JOUEUR"
-    while jouer:
-        while nombre_allumettes > 0:
+def game():
+    play = True
+    while play:
+        board = []
+        nb_matches = 8
+        print("Qui va jouer en premier ? (1 pour l'ordinateur, 2 pour vous)")
+        choice = input()
+        if choice == "1":
+            player = "MACHINE"
+        else:
+            player = "JOUEUR"
+        while nb_matches > 0:
             if player == "JOUEUR":
                 print("Combien d'allumettes voulez-vous retirer ? (1 ou 2)")
+                choice_matches = int(input())
+                while choice_matches > nb_matches:
+                    if choice_matches not in [1, 2]:
+                        print("Vous devez choisir 1 ou 2 allumettes.")
+                    elif choice_matches > nb_matches:
+                        print("Il ne reste que", nb_matches, "allumette(s) !")
+                    choice_matches = int(input("Combien d'allumettes voulez-vous retirer ? (1 ou 2) "))
+                nb_matches-=choice_matches
+                board = update(nb_matches)
+                print(board)
                 player = "MACHINE"
 
             elif player == "MACHINE":
                 print("L'ordinateur joue...")
                 player = "JOUEUR"
-            board = update(nombre_allumettes)
-            print(board)
-            print("Il reste " + str(nombre_allumettes) + " allumettes.")
-    print("Le gagnant est " + player + " !")
-    print("Voulez-vous rejouer ? (o/n)")
-    reponse = input().lower()
-    if reponse == "o":
-        jouer = True
-    else:
-        jouer = False
-
+                board = update(nb_matches)
+                print(board)
+            print("Il reste " + str(nb_matches) + " allumettes.")
+        print("Le gagnant est " + player + " !")
+        print("Voulez-vous rejouer ? (o/n)")
+        response = input().lower()
+        if response == "o":
+            play = True
+        else:
+            play = False
     return
+
+
+game()

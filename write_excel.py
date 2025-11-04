f87ddd95ec094ac4917e6d632becee307c6d0c52
ALVARES-Titouan-2326003b <titouan.alvares@etu.univ-amu.fr>
2025-11-04 15:09:54 +0100

    Ajout de l'affichage des étapes de chaque partie, du formatage pour l'affichage des gobelets réinitialisés
    Modif du code pour que l'affichage s'adapte selon le nombre de coups possibles (avant l'affichage n'était adapté que pour 2 coups possibles)/
    Préparation de l'affichage des proba de chaque coup par étape selon le joueur après chaque partie.

--- write_excel.py
+++ write_excel.py
@@ -18,13 +18,13 @@ games_titles_format = workbook.add_format({
     "align": "center",
     "valign": "vcenter"
 })
-bold = workbook.add_format({
-    "bold": 1
+no_step = workbook.add_format({
+    "bg_color": "black"
 })
 
 
 matches = 8
-take = {"red": 1, "yellow": 2}
+take = {"red": 1, "yellow": 2, "blue": 4}
 default_proba = 0.5
 default_count = 5
 recompense = 2
@@ -49,43 +49,78 @@ worksheet.write(5, states_offset-1, "état", glob_titles_format)
 for i in range(matches):
     worksheet.write(5, states_offset+i, 8-i, glob_titles_format)
 
+nb_take = len(take)
 games_offset = 7
-game_height = 6
+game_height = 2 + 2*nb_take
 
 def add_game(results, steps_p1, steps_p2, cups_p1, cups_p2, cups_reseted_p1 = [], cups_reseted_p2 = []):
     first_row = games_offset+game_height*(game_counter-1)
     last_row = games_offset+game_height*game_counter-1
+    
     worksheet.merge_range(
         first_row, 0,
         last_row, 0,
         game_counter, games_titles_format
     )
+    
     worksheet.write(first_row, 1, "P1", games_titles_format)
     worksheet.write(first_row+1, 1, "P2", games_titles_format)
+    
     worksheet.merge_range(
         first_row+2, 1,
-        first_row+3, 1,
+        first_row+1+nb_take, 1,
         "P1", games_titles_format
     )
     worksheet.merge_range(
-        first_row+4, 1,
-        first_row+5, 1,
+        first_row+2+nb_take, 1,
+        first_row+1+nb_take*2, 1,
         "P2", games_titles_format
     )
     
-    worksheet.write(first_row, 3, json.dumps(cups_reseted_p1) if len(cups_reseted_p1) > 0 else "-")
-    worksheet.write(first_row+1, 3, json.dumps(cups_reseted_p2) if len(cups_reseted_p2) > 0 else "-")
+    players = list(results.keys())
+    worksheet.merge_range(
+        first_row+2, 2,
+        first_row+1+nb_take, 2,
+        results["P1"] if "P1" in players else "no data", games_titles_format
+    )
+    worksheet.merge_range(
+        first_row+2+nb_take, 2,
+        first_row+1+nb_take*2, 2,
+        results["P2"] if "P2" in players else "no data", games_titles_format
+    )
+    
+    worksheet.write(first_row, 3, json.dumps(cups_reseted_p1) if len(cups_reseted_p1) > 0 else "-", glob_values_format)
+    worksheet.write(first_row+1, 3, json.dumps(cups_reseted_p2) if len(cups_reseted_p2) > 0 else "-", glob_values_format)
+    
+    counter = 0
+    for nb_matches in take.values():
+        counter += 1
+        worksheet.write(first_row+1+counter, 4, nb_matches)
+        worksheet.write(first_row+1+counter+nb_take, 4, nb_matches)
+    
+    for step in steps_p1:
+        worksheet.write(first_row, states_offset+matches-1-step[0], take[step[1]], glob_values_format)
+    
+    for step in steps_p2:
+        worksheet.write(first_row+1, states_offset+matches-1-step[0], take[step[1]], glob_values_format)
+    
+    worksheet.conditional_format(first_row, states_offset, first_row+1, states_offset+7, {
+        'type':'blanks',
+        'format': no_step
+    })
+    
+    
 
 
     
 game_counter += 1
-add_game({"P1": "gagne", "P2": "perd"}, [[5, 'red'], [2, 'red']], [[7, 'red'], [3, 'yellow']], [], [])
+add_game({"P1": "gagne", "P2": "perd"}, [[7, 'red'], [4, 'yellow'], [1, 'yellow']], [[6, 'yellow'], [2, 'red']], [], [])
 game_counter += 1
-add_game({"P1": "gagne", "P2": "perd"}, [[5, 'red'], [2, 'red']], [[7, 'red'], [3, 'yellow']], [], [], cups_reseted_p1 = ["8", "3"])
+add_game({"P2": "perd"}, [[7, 'red'], [4, 'yellow'], [1, 'yellow']], [[6, 'yellow'], [2, 'red']], [], [], cups_reseted_p1 = ["8", "3"])
 game_counter += 1
-add_game({"P1": "gagne", "P2": "perd"}, [[5, 'red'], [2, 'red']], [[7, 'red'], [3, 'yellow']], [], [], cups_reseted_p1 = ["8", "3"], cups_reseted_p2 = ["5", "2"])
+add_game({"P1": "gagne"}, [[7, 'red'], [4, 'yellow'], [1, 'yellow']], [[6, 'yellow'], [2, 'red']], [], [], cups_reseted_p1 = ["8", "3"], cups_reseted_p2 = ["5", "2"])
 game_counter += 1
-add_game({"P1": "gagne", "P2": "perd"}, [[5, 'red'], [2, 'red']], [[7, 'red'], [3, 'yellow']], [], [], cups_reseted_p2 = ["5", "2"])
+add_game({}, [[7, 'red'], [4, 'yellow'], [1, 'yellow']], [[6, 'yellow'], [2, 'red']], [], [], cups_reseted_p2 = ["5", "2"])
 
 
 

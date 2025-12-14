61b68a692a0a09de9f771398626f4f8cff553e9c
GRIMAUD-Estelle-2326056aa <estelle.grimaud@etu.univ-amu.fr>
2025-12-14 18:04:38 +0100

    Correction implémentation graphe avec variables en anglais

--- game_graph.py
+++ game_graph.py
@@ -16,7 +16,7 @@ def read_txt(fichier):
 
 # Matrice d'adjacence
 
-def matrice_adj(result):
+def adjacency_matrix(result):
     mat = np.zeros((len(result)+1, len(result)+1), dtype=int)
     for cup in range(len(result)):
         for color in range(len(result[cup])):
@@ -26,33 +26,34 @@ def matrice_adj(result):
                 mat[cup][cup+2] += 1
     return mat
 
-# Matrice d'adjacence avec probabilités
+# Matrice de transition
 
-def matrice_proba(result, mat_adj):
+def transition_matrix(result, adj_mat):
 
     # Initialisation d'une matrice nulle
-    mat_proba = np.zeros((len(result)+1, len(result)+1), dtype=float)
+    mat = np.zeros((len(result)+1, len(result)+1), dtype=float)
 
     # Parcours de la matrice d'adjacence
     for row in range(len(result)):
         # Degré sortant utilisé pour calculer les probabilités
         sum_row = len(result[row])
         # Parcours de la nouvelle matrice
-        for col in range(len(mat_adj)):
+        for col in range(len(adj_mat)):
             if sum_row != 0:
-                mat_proba[row][col] = round(mat_adj[row][col]/sum_row, 2)
+                mat[row][col] = round(adj_mat[row][col]/sum_row, 2)
 
-    return mat_proba
+    return mat
 
 # Fonction d'affichage du graphe de manière linéaire
 
-def graphe_trace_lineaire(G, couleurs):
+def draw_linear_graph(G, colors, title):
 
-    plt.figure(figsize=(14, 5)) 
+    # Définition de la taille de la figure
+    plt.figure(figsize=(14, 5))
 
     # Définition de la disposition linéaire
-    ordre = sorted(G.nodes(), key=lambda x: int(x), reverse=True)
-    pos = {node: (i, 0) for i, node in enumerate(ordre)}
+    sorted_nodes = sorted(G.nodes(), key=lambda x: int(x), reverse=True)
+    pos = {node: (i, 0) for i, node in enumerate(sorted_nodes)}
 
     # Tracé des arêtes (avec courbure pour les transitions de 2 en 2)
     for u, v in G.edges():
@@ -73,7 +74,7 @@ def graphe_trace_lineaire(G, couleurs):
         )
 
     # Tracé des nœuds avec labels
-    nx.draw_networkx_nodes(G, pos, node_color=couleurs, node_size=350)
+    nx.draw_networkx_nodes(G, pos, node_color=colors, node_size=350)
     nx.draw_networkx_labels(G, pos, font_size=10)
 
     # Tracé des labels des arêtes
@@ -91,11 +92,11 @@ def graphe_trace_lineaire(G, couleurs):
 
     # Tracé des labels pour les arêtes de 1 en 1 (droites)
     nx.draw_networkx_edge_labels(
-        G, 
-        pos, 
-        edge_labels=edge_labels_straight, 
-        font_size=8, 
-        rotate=False, 
+        G,
+        pos,
+        edge_labels=edge_labels_straight,
+        font_size=8,
+        rotate=False,
         label_pos=0.5
     )
 
@@ -124,42 +125,42 @@ def graphe_trace_lineaire(G, couleurs):
         )
 
     # Légende des sommets 
-    sommets_noyau = mpatches.Patch(color='red', label='sommet dans le noyau')
-    sommets_simples = mpatches.Patch(color='lightblue', label='sommet hors du noyau')
+    kernel_nodes = mpatches.Patch(color='red', label='sommet dans le noyau')
+    simple_nodes = mpatches.Patch(color='lightblue', label='sommet hors du noyau')
 
     # Ajustement des limites de l'axe Y
     ax = plt.gca()
     ax.set_ylim(-0.5, 0.7)
 
     # Paramétrage et affichage du graphe
-    plt.title("Graphe des états du Jeu de Nim")
-    plt.legend(handles=[sommets_noyau, sommets_simples], loc='lower right')
+    plt.title(title)
+    plt.legend(handles=[kernel_nodes, simple_nodes], loc='lower right')
     plt.axis("off")
     plt.show()
 
 # Création du graphe avec recherche des sommets dans le noyau
 
-def graphe_noyau(mat_proba):
+def kernel_graph(trans_matrix):
 
-    # Création graphe orienté
+    # Création du graphe orienté
     G = nx.DiGraph()
+  
+    n = len(trans_matrix)
+    
+    # Définition des noms des sommets
+    nodes = [str(n - i - 1) for i in range(n)]
 
-    # Définition des noms des sommets : nb d'allumettes restantes
-    sommets = []
-    for i in range (len(mat_proba)):
-        sommets.append(str(len(mat_proba)-i-1))
-
-    # Ajouter les arêtes à partir de la matrice
-    for i in range(len(mat_proba)):
-        for j in range(len(mat_proba)):
-            poids = mat_proba[i][j]
-            if poids != 0:
-                G.add_edge(sommets[i], sommets[j], weight=poids)
+    # Ajout des arêtes à partir de la matrice
+    for i in range(n):
+        for j in range(n):
+            weight = trans_matrix[i][j]
+            if weight!= 0:
+                G.add_edge(nodes[i], nodes[j], weight=weight)
 
     grundy = {}
 
     # Parcours et définition de la valeur pour chaque sommet
-    for s in sorted(sommets, key=int):
+    for s in sorted(nodes, key=int):
         succ = list(G.successors(s))
 
         # État terminal = 0
@@ -176,17 +177,14 @@ def graphe_noyau(mat_proba):
         grundy[s] = mex
 
     # Définition des couleurs pour chaque sommet en fonction de la valeur de grundy
-    couleurs = []
+    colors = []
     for sommet in G.nodes():
         if grundy[sommet] == 0 :
-            couleurs.append("red")
+            colors.append("red")
         else :
-            couleurs.append("lightblue")
+            colors.append("lightblue")
 
-    # Tracé du graphe
-    graphe_trace_lineaire(G, couleurs)
-
-    return G, couleurs
+    return G, colors
 
 def main():
 
@@ -194,14 +192,17 @@ def main():
     result = read_txt("apprentissage_par_renforcement_jeu_de_nim/results/11_1_2000.txt")
 
     # Affichage matrice d'adjacence
-    print("Matrice d'adjacence : ", matrice_adj(result))
+    print("Matrice d'adjacence : ", adjacency_matrix(result))
 
-    # Affichage matrice d'adjacence avec probabilités
-    mat_proba = matrice_proba(result, matrice_adj(result))
-    print("Matrice d'adjacence avec probabilités : ", mat_proba)
+    # Affichage matrice de transition
+    trans_matrix = transition_matrix(result, adjacency_matrix(result))
+    print("Matrice de transition : ", trans_matrix)
+
+    # Création du graphe avec recherche des sommets dans le noyau
+    G, colors = kernel_graph(trans_matrix)
 
     # Affichage du graphe à partir de la matrice d'adjacence avec les probabilités
     # Avec mise en évidence des sommets dans le noyau
-    G_noyau = graphe_noyau(mat_proba)
+    draw_linear_graph(G, colors, "Graphe des probabilités de jeu de la machine 1 après 2000 parties d'entraînement avec mise en évidence du noyau")
 
 main()
